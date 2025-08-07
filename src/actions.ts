30358df6c46f9ac67ea86f91cbb31d27574e6df2
Clay Avery <clay@claytoncreative.com>
2025-08-07 09:14:47 -0500

    Added some more actions
    Get Device Info
    Set IP Address
    Set Machine Name

--- src/actions.ts
+++ src/actions.ts
@@ -1,10 +1,101 @@
 import { ModuleInstance } from './main.js'
 import * as http from 'http'
 
+export function httpSend(self: ModuleInstance, host: string, port: number, url: string): void {
+	const options = {
+		hostname: host,
+		port: port || 80,
+		path: url,
+		method: 'GET',
+	}
+
+	const req = http.request(options, (res) => {
+		let data = ''
+		res.on('data', (chunk) => {
+			data += chunk
+		})
+		res.on('end', () => {
+			self.log('info', `HTTP Response: ${data}`)
+		})
+	})
+
+	req.on('error', (err) => {
+		self.log('error', `HTTP Request failed: ${err.message}`)
+	})
+
+	req.end()
+}
+
 export function UpdateActions(self: ModuleInstance): void {
+	const labels_tooltip_str =
+		'Cannot contain spaces or any of the following characters: ' +
+		'< > ! @ # & $ % ^ & * " ' +
+		"' " +
+		'` / ' +
+		'\\ ' +
+		', . : ; ? ='
+
 	self.setActionDefinitions({
-		route_action: {
-			name: 'Routing',
+		get_info_action: {
+			options: [
+				{
+					id: 'get_info_str',
+					type: 'static-text',
+					label: '',
+					value: '',
+				},
+			],
+			name: 'Get Device Info',
+			callback: async function () {
+				const ip = self.config.host
+				const port = self.config.port
+				const url = '/cgi-bin/command.cgi?cmd=Info&param=[{"func":"get","type":"device"}]'
+				httpSend(self, ip, port, url)
+			},
+		},
+
+		set_ip_action: {
+			options: [
+				{
+					id: 'set_ip_str',
+					type: 'textinput',
+					label: 'IP Address',
+					default: self.config.host,
+				},
+			],
+			name: 'Set IP Address',
+			callback: async function (event) {
+				const ip = self.config.host
+				const port = self.config.port
+				const url =
+					'/cgi-bin/command.cgi?cmd=Info&param=[{"func":"set","type":"ip","value":"' + event.options.set_ip_str + '"}]'
+				httpSend(self, ip, port, url)
+			},
+		},
+
+		set_machinename_action: {
+			options: [
+				{
+					id: 'set_machinename_str',
+					type: 'textinput',
+					label: 'Machine Name',
+					tooltip: labels_tooltip_str,
+				},
+			],
+			name: 'Set Machine Name',
+			callback: async function (event) {
+				const ip = self.config.host
+				const port = self.config.port
+				const url =
+					'/cgi-bin/command.cgi?cmd=Info&param=[{"func":"set","type":"name","value":"' +
+					event.options.set_machinename_str +
+					'"}]'
+				httpSend(self, ip, port, url)
+			},
+		},
+
+		set_route_action: {
+			name: 'Set Route',
 			options: [
 				{
 					id: 'input_str',
@@ -22,78 +113,36 @@ export function UpdateActions(self: ModuleInstance): void {
 			],
 			callback: async function (event) {
 				const ip = self.config.host
-				const urlPath =
+				const port = self.config.port
+				const url =
 					'/cgi-bin/command.cgi?cmd=Route&param=[{"func":"set","route":[{"input":' +
 					event.options.input_str +
 					',"output":[' +
 					event.options.output_str +
 					']}]}]'
-
-				const options = {
-					hostname: ip,
-					port: self.config.port || 80,
-					path: urlPath,
-					method: 'GET',
-				}
-
-				const req = http.request(options, (res) => {
-					let data = ''
-					res.on('data', (chunk) => {
-						data += chunk
-					})
-					res.on('end', () => {
-						self.log('info', `Response: ${data}`)
-					})
-				})
-
-				req.on('error', (err) => {
-					self.log('error', `HTTP Request failed: ${err.message}`)
-				})
-
-				req.end()
+				httpSend(self, ip, port, url)
 			},
 		},
 
-		salvo_action: {
-			name: 'Salvo',
+		set_salvo_action: {
+			name: 'Set Salvo',
 			options: [
 				{
-					id: 'salvo_str',
+					id: 'set_salvo_str',
 					type: 'textinput',
 					label: 'Salvo Name',
 				},
 			],
+
 			callback: async function (event) {
 				const ip = self.config.host
-				const urlPath =
-					'/cgi-bin/command.cgi?cmd=Salvo&param=[{"func":"take","name":"' + event.options.salvo_str + '"}]'
-
-				const options = {
-					hostname: ip,
-					port: self.config.port || 80,
-					path: urlPath,
-					method: 'GET',
-				}
-
-				const req = http.request(options, (res) => {
-					let data = ''
-					res.on('data', (chunk) => {
-						data += chunk
-					})
-					res.on('end', () => {
-						self.log('info', `Response: ${data}`)
-					})
-				})
-
-				req.on('error', (err) => {
-					self.log('error', `HTTP Request failed: ${err.message}`)
-				})
-
-				req.end()
+				const port = self.config.port
+				const url = '/cgi-bin/command.cgi?cmd=Salvo&param=[{"func":"take","name":"' + event.options.salvo_str + '"}]'
+				httpSend(self, ip, port, url)
 			},
 		},
 
-		labels_action: {
+		set_labels_action: {
 			name: 'Set Labels',
 			options: [
 				{
@@ -101,161 +150,91 @@ export function UpdateActions(self: ModuleInstance): void {
 					type: 'textinput',
 					label: 'Port 1',
 					default: 'Port_1',
-					tooltip:
-						'Cannot contain spaces or any of the following characters: ' +
-						'< > ! @ # & $ % ^ & * " ' +
-						"' " +
-						'` / ' +
-						'\\ ' +
-						', . : ; ? =',
+					tooltip: labels_tooltip_str,
 				},
 				{
 					id: 'port2_str',
 					type: 'textinput',
 					label: 'Port 2',
 					default: 'Port_2',
-					tooltip:
-						'Cannot contain spaces or any of the following characters: ' +
-						'< > ! @ # & $ % ^ & * " ' +
-						"' " +
-						'` / ' +
-						'\\ ' +
-						', . : ; ? =',
+					tooltip: labels_tooltip_str,
 				},
 				{
 					id: 'port3_str',
 					type: 'textinput',
 					label: 'Port 3',
 					default: 'Port_3',
-					tooltip:
-						'Cannot contain spaces or any of the following characters: ' +
-						'< > ! @ # & $ % ^ & * " ' +
-						"' " +
-						'` / ' +
-						'\\ ' +
-						', . : ; ? =',
+					tooltip: labels_tooltip_str,
 				},
 				{
 					id: 'port4_str',
 					type: 'textinput',
 					label: 'Port 4',
 					default: 'Port_4',
-					tooltip:
-						'Cannot contain spaces or any of the following characters: ' +
-						'< > ! @ # & $ % ^ & * " ' +
-						"' " +
-						'` / ' +
-						'\\ ' +
-						', . : ; ? =',
+					tooltip: labels_tooltip_str,
 				},
 				{
 					id: 'port5_str',
 					type: 'textinput',
 					label: 'Port 5',
 					default: 'Port_5',
-					tooltip:
-						'Cannot contain spaces or any of the following characters: ' +
-						'< > ! @ # & $ % ^ & * " ' +
-						"' " +
-						'` / ' +
-						'\\ ' +
-						', . : ; ? =',
+					tooltip: labels_tooltip_str,
 				},
 				{
 					id: 'port6_str',
 					type: 'textinput',
 					label: 'Port 6',
 					default: 'Port_6',
-					tooltip:
-						'Cannot contain spaces or any of the following characters: ' +
-						'< > ! @ # & $ % ^ & * " ' +
-						"' " +
-						'` / ' +
-						'\\ ' +
-						', . : ; ? =',
+					tooltip: labels_tooltip_str,
 				},
 				{
 					id: 'port7_str',
 					type: 'textinput',
 					label: 'Port 7',
 					default: 'Port_7',
-					tooltip:
-						'Cannot contain spaces or any of the following characters: ' +
-						'< > ! @ # & $ % ^ & * " ' +
-						"' " +
-						'` / ' +
-						'\\ ' +
-						', . : ; ? =',
+					tooltip: labels_tooltip_str,
 				},
 				{
 					id: 'port8_str',
 					type: 'textinput',
 					label: 'Port 8',
 					default: 'Port_8',
-					tooltip:
-						'Cannot contain spaces or any of the following characters: ' +
-						'< > ! @ # & $ % ^ & * " ' +
-						"' " +
-						'` / ' +
-						'\\ ' +
-						', . : ; ? =',
+					tooltip: labels_tooltip_str,
 				},
 				{
 					id: 'port9_str',
 					type: 'textinput',
 					label: 'Port 9',
 					default: 'Port_9',
-					tooltip:
-						'Cannot contain spaces or any of the following characters: ' +
-						'< > ! @ # & $ % ^ & * " ' +
-						"' " +
-						'` / ' +
-						'\\ ' +
-						', . : ; ? =',
+					tooltip: labels_tooltip_str,
 				},
 				{
 					id: 'port10_str',
 					type: 'textinput',
 					label: 'Port 10',
 					default: 'Port_10',
-					tooltip:
-						'Cannot contain spaces or any of the following characters: ' +
-						'< > ! @ # & $ % ^ & * " ' +
-						"' " +
-						'` / ' +
-						'\\ ' +
-						', . : ; ? =',
+					tooltip: labels_tooltip_str,
 				},
 				{
 					id: 'port11_str',
 					type: 'textinput',
 					label: 'Port 11',
 					default: 'Port_11',
-					tooltip:
-						'Cannot contain spaces or any of the following characters: ' +
-						'< > ! @ # & $ % ^ & * " ' +
-						"' " +
-						'` / ' +
-						'\\ ' +
-						', . : ; ? =',
+					tooltip: labels_tooltip_str,
 				},
 				{
 					id: 'port12_str',
 					type: 'textinput',
 					label: 'Port 12',
 					default: 'Port_12',
-					tooltip:
-						'Cannot contain spaces or any of the following characters: ' +
-						'< > ! @ # & $ % ^ & * " ' +
-						"' " +
-						'` / ' +
-						'\\ ' +
-						', . : ; ? =',
+					tooltip: labels_tooltip_str,
 				},
 			],
+
 			callback: async function (event) {
 				const ip = self.config.host
-				const urlPath =
+				const port = self.config.port
+				const url =
 					'/cgi-bin/command.cgi?cmd=Info&param=[{"func":"set","type":"genlabel","sib_label":[{"port":1,"label":"' +
 					event.options.port1_str +
 					'"},{"port":2,"label":"' +
@@ -281,29 +260,7 @@ export function UpdateActions(self: ModuleInstance): void {
 					'"},{"port":12,"label":"' +
 					event.options.port12_str +
 					'"}]}]'
-
-				const options = {
-					hostname: ip,
-					port: self.config.port || 80,
-					path: urlPath,
-					method: 'GET',
-				}
-
-				const req = http.request(options, (res) => {
-					let data = ''
-					res.on('data', (chunk) => {
-						data += chunk
-					})
-					res.on('end', () => {
-						self.log('info', `Response: ${data}`)
-					})
-				})
-
-				req.on('error', (err) => {
-					self.log('error', `HTTP Request failed: ${err.message}`)
-				})
-
-				req.end()
+				httpSend(self, ip, port, url)
 			},
 		},
 	})
